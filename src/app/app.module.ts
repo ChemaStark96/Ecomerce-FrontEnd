0d21f4f934d43aebeea75fbcec2e226bee93bdc5
Jose Maria Vazquez Garcia <jose.vazquez@brivesoluciones.com>
2025-04-22 12:58:07 -0600

    Se crean los componentes de clientes, nav bar, pedidos y productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
+import { ClientesComponent } from './components/clientes/clientes/clientes.component';
+import { PedidosComponent } from './components/pedidos/pedidos/pedidos.component';
+import { ProductosComponent } from './components/productos/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ClientesComponent,
+    PedidosComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
