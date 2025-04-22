0b111746fc367fb160b7bce83d54193b4967b422
Jose Maria Vazquez Garcia <jose.vazquez@brivesoluciones.com>
2025-04-22 14:13:51 -0600

    Se corrigen app routing y app component para que el proyecto pueda levantar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/common/navbar/navbar.component';
-import { ClientesComponent } from './components/clientes/clientes/clientes.component';
-import { PedidosComponent } from './components/pedidos/pedidos/pedidos.component';
-import { ProductosComponent } from './components/productos/productos/productos.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 @NgModule({
   declarations: [
