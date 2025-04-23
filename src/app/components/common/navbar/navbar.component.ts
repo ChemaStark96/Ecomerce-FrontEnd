eae6f68466a456a0394f34b07f8a2ec46a197d83
Brandon Ramos <brandonhastur@gmail.com>
2025-04-22 23:49:57 -0600

    Navbar creada con estilos y estilos globales agregados
    😃😃😃

--- src/app/components/common/navbar/navbar.component.ts
+++ src/app/components/common/navbar/navbar.component.ts
@@ -6,6 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
 
+export class NavbarComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  
 }
