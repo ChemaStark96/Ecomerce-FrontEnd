7a3443a19b23a235be486d77c9b43e82d7c9dd14
Brandon Ramos <brandonhastur@gmail.com>
2025-04-23 12:18:13 -0600

    Servicio de Productos
    esta lo basico para que se pueda usar 🥴

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Producto } from '../models/Producto.model';
+import { enviroment } from '../enviroments/enviroment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
 
-  constructor() { }
+  private apiUrl: string = enviroment.apiUrl +'productos/';  
+
+  constructor(private http: HttpClient) { }
+
+  getProductos(): Observable<Producto[]>{
+    return this.http.get<Producto[]>(this.apiUrl);
+  }
+  
+  postProductos(producto: Producto): Observable<Producto>{
+    return this.http.post<Producto>(this.apiUrl,producto);
+  }
+
+  putProductos(producto: Producto): Observable<Producto>{
+     return this.http.put<Producto>(`${this.apiUrl}${producto.id}`,producto);
+  }
+
+  deleteProductos(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}${id}`);
+  }
+  
 }
